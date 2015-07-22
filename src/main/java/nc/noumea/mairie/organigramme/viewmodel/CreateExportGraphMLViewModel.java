0fb157292839f705f57e6d567fe51b04a5949d87
ThomasLemaire <thomas.lemaire@agilesoft.nc>
2015-07-23 10:53:29 +1100

    Visualiser un organigramme d'entités avec les fiches de poste dans l'export Yed

---
+++ src/main/java/nc/noumea/mairie/organigramme/viewmodel/CreateExportGraphMLViewModel.java
@@ -0,0 +1,78 @@
+package nc.noumea.mairie.organigramme.viewmodel;
+
+/*
+ * #%L
+ * Logiciel de Gestion des Organigrammes de la Ville de Nouméa
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2015 Mairie de Nouméa
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import nc.noumea.mairie.organigramme.core.viewmodel.AbstractPopupViewModel;
+import nc.noumea.mairie.organigramme.dto.ExportDto;
+
+import org.zkoss.bind.BindUtils;
+import org.zkoss.bind.annotation.AfterCompose;
+import org.zkoss.bind.annotation.BindingParam;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.ContextParam;
+import org.zkoss.bind.annotation.ContextType;
+import org.zkoss.bind.annotation.GlobalCommand;
+import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.Selectors;
+import org.zkoss.zk.ui.select.annotation.VariableResolver;
+import org.zkoss.zkplus.spring.DelegatingVariableResolver;
+import org.zkoss.zul.Window;
+
+@Init(superclass = true)
+@VariableResolver(DelegatingVariableResolver.class)
+public class CreateExportGraphMLViewModel extends AbstractPopupViewModel<ExportDto> implements Serializable {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@AfterCompose
+	public void afterCompose(@ContextParam(ContextType.VIEW) Component view) {
+		setPopup((Window) Selectors.iterable(view, "#createExportGraphML").iterator().next());
+	}
+
+	@GlobalCommand
+	@NotifyChange("entity")
+	public void ouvrePopupCreationExport(@BindingParam("exportDto") ExportDto exportDto) {
+		setEntity(exportDto);
+		getPopup().doModal();
+	}
+
+	/**
+	 * Export graphML
+	 */
+	@Command
+	@NotifyChange("entity")
+	public void export() {
+		final Map<String, Object> mapExportDto = new HashMap<String, Object>();
+		mapExportDto.put("exportDto", this.entity);
+		BindUtils.postGlobalCommand(null, null, "exportGraphMLFromEntite", mapExportDto);
+		getPopup().detach();
+	}
+}
