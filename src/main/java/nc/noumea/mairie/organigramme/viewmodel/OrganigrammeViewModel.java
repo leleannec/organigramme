673cb94d305c103723935f6a39714ea8e554aed8
ThomasLemaire <thomas.lemaire@agilesoft.nc>
2015-08-13 15:26:28 +1100

    Anomalie export YED (démo sprint 3) : les noeuds ne sont pas tous exportés : il semble que seul un niveau de profondeur est exporté

--- src/main/java/nc/noumea/mairie/organigramme/viewmodel/OrganigrammeViewModel.java
+++ src/main/java/nc/noumea/mairie/organigramme/viewmodel/OrganigrammeViewModel.java
@@ -150,7 +150,7 @@ public class OrganigrammeViewModel extends AbstractViewModel<EntiteDto> implemen
 	 * Map permettant de savoir si le Li est ouvert ou non à partir de son id
 	 * html client
 	 **/
-	Map<String, Boolean> mapIdLiOuvert;
+	Map<String, Boolean> mapIdLiOuvert = new HashMap<String, Boolean>();
 
 	/** Le currentUser connecté **/
 	ProfilAgentDto profilAgentDto;
@@ -228,7 +228,9 @@ public class OrganigrammeViewModel extends AbstractViewModel<EntiteDto> implemen
 		// permet de gérer le onClickEntite
 		Selectors.wireEventListeners(view, this);
 
-		mapIdLiOuvert = new HashMap<String, Boolean>();
+		for (EntiteDto entiteDto : getAllEntiteAPlat()) {
+			mapIdLiOuvert.put("entite-id-" + entiteDto.getId().toString(), false);
+		}
 		vlayout = (Vlayout) Selectors.iterable(view, "#organigramme").iterator().next();
 		profilAgentDto = authentificationService.getCurrentUser();
 
@@ -314,7 +316,6 @@ public class OrganigrammeViewModel extends AbstractViewModel<EntiteDto> implemen
 		boolean ouvert = mapIdLiOuvert.get(entiteDto.getIdLi()) != null ? !mapIdLiOuvert.get(entiteDto.getIdLi())
 				: false;
 		mapIdLiOuvert.put(entiteDto.getIdLi(), ouvert);
-		setLiOuvertOuFermeArbre(entiteDto.getEnfants(), ouvert);
 	}
 
 	/**
